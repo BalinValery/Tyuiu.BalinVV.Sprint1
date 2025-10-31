055b7a4a8134d05813348f0c2ec625f5ff3556f0
BalinValery <valera.balin00@mail.ru>
2025-11-01 03:23:10 +0500

    Task5

---
+++ Tyuiu.BalinVV.Sprint1.Task5.V5.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BalinVV.Sprint1.Task5.V5.Lib;
+namespace Tyuiu.BalinVV.Sprint1.Task5.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 32.597;
+            var res = ds.Calculate(x);
+            var z = 5;
+            Assert.AreEqual(z, res);
+        }
+    }
+}
---
+++ Tyuiu.BalinVV.Sprint1.Task5.V5/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.BalinVV.Sprint1.Task5.V5.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выполнил Балин В. В. | СМАРТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                                ");
+        Console.WriteLine("* Тема: Преобразование типов и класс Convert                               ");
+        Console.WriteLine("* Задание #5");
+        Console.WriteLine("* Вариант #5");
+        Console.WriteLine("* Выполнил Балин В. В. | СМАРТб-25-1");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:");
+        Console.WriteLine("* Присвоить целой переменной d первую цифру из дробной части положительного");
+        Console.WriteLine("*вещественного числа x                                                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        double x;
+        Console.WriteLine("Введите дробное число X:                                                  *");
+        x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Первая цифра дробной части = " + ds.Calculate(x));
+    }
+}
