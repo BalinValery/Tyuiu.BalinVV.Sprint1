65c590800ae8ea5a8a99a3740b4d4fde80355b7a
BalinValery <valera.balin00@mail.ru>
2025-11-01 03:35:33 +0500

    Task6

---
+++ Tyuiu.BalinVV.Sprint1.Task6.V1.Test/DataSerciceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.BalinVV.Sprint1.Task6.V1.Lib;
+namespace Tyuiu.BalinVV.Sprint1.Task6.V1.Test
+{
+    [TestClass]
+    public sealed class DataSerciceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.SymbolCode("1");
+            Assert.AreEqual("49", res);
+        }
+    }
+}
+
---
+++ Tyuiu.BalinVV.Sprint1.Task6.V1/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.BalinVV.Sprint1.Task6.V1.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнил: Балин В. В. | СМАРТб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Работа со строками класс String                                   *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #1                                                              *");
+        Console.WriteLine("* Выполнил:             Балин В. В. | СМАРТб-25-1                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*Напишите программу, которая выводит код введенного пользователем символа *");
+        Console.WriteLine("* Программа должна завершать работу в результате ввода, например, точки.  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите символ:");
+        string x = Console.ReadLine();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.SymbolCode(x));
+        Console.ReadKey();
+    }
+}
