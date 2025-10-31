d08bba23a4b69df1c7414d9d99245c80eea05fc6
BalinValery <valera.balin00@mail.ru>
2025-11-01 02:06:50 +0500

    Task3

---
+++ Tyuiu.BalinVV.Sprint1.Task3.V10.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.BalinVV.Sprint1.Task3.V10.Lib
+{
+    public class DataService : ISprint1Task3V10
+    {
+        public string NumberToMoney(double number)
+        {
+            number = Math.Round(number, 3);
+            int rubles = 0;
+            int kopeiki = 0;
+
+            rubles = (int)number;
+            double ost = (number - rubles) * 100;
+            kopeiki = (int)ost;
+
+
+            return $"{number} руб. - это {rubles} руб. {kopeiki} коп.";
+        }
+    }
+}
---
+++ Tyuiu.BalinVV.Sprint1.Task3.V10.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.BalinVV.Sprint1.Task3.V10.Lib;
+namespace Tyuiu.BalinVV.Sprint1.Task3.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpressions()
+        {
+            var ds = new DataService();
+            Assert.AreEqual(ds.NumberToMoney(236), "23.6 руб. — это 23 руб. 60 коп.");
+        }
+    }
+}
+
